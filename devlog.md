c9c404380f9e667c562633ed695f5588af2bd621
mzigor <none@mail.com>
2025-06-27 23:04:19 -0300

    Devlog created, functions for initial user prompts

---
+++ devlog.md
@@ -0,0 +1,27 @@
+# Log de desenvolvimento
+
+Preciso desenvolver as seguintes funcionalidades:
+
+- [ ] Cadastrar voos na fila de espera;
+
+- [ ] Cadastrar nomes na lista de passageiros de um voo (o usuário deve informar 
+o  ID do voo desejado);
+
+- [ ] Remover nomes da lista de passageiros de um voo (o usuário deve informar o 
+ID do voo e nome desejado);
+
+- [ ] Listar todas as características do primeiro avião da fila (exceto a lista 
+de passageiros);
+
+- [ ] Autorizar a decolagem do primeiro avião da fila (removendo-o da fila). Não 
+esquecer de remover também a lista de passageiros;
+
+- [ ] Listar o número de aviões aguardando na fila de decolagem;
+
+- [ ] Listar os dados de todos os aviões na fila de espera (um em cada linha da 
+tela, exceto a lista de passageiros), conforme modelo;
+
+- [ ] Listar na tela os passageiros de um determinado voo em ordem alfabética e 
+informar no final a quantidade de passageiros no voo;
+
+- [ ] Sair do programa (liberar a lista de passageiros e voos antes de finalizar).
--- src.c
+++ src.c
@@ -21,6 +21,61 @@
 ** Ferramenta Interativa para Controle de Tráfego em Aeroportos
 */
 
+// User interaction-------------------------------------------------------------
+
+char *
+io_str_input(){
+    char *p_input = (char *)malloc(sizeof(char)*100);
+
+    fgets(p_input, (sizeof(p_input)), stdin);
+
+    // Finding out the received string's size
+    size_t len = strlen(p_input);
+
+    // Removing an eventual newline at the end of `p_input`
+    if (len > 0 && p_input[len - 1] == '\n') {
+        p_input[len - 1] = '\0';
+    }
+
+    // Cleaning up any remaining input in `stdin`
+    // int c;
+    // while ((c = getchar()) != '\n' && c != EOF){}
+
+    return p_input;
+}
+
+void
+io_startup(void)
+{
+    printf("\t░▒▓████████▓▒░▒▓█▓▒░░▒▓██████▓▒░▒▓████████▓▒░▒▓██████▓▒░ \n");
+    printf("\t░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░\n");
+    printf("\t░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░\n");
+    printf("\t░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░  ░▒▓████████▓▒░\n");
+    printf("\t░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░        ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░\n");
+    printf("\t░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░\n");
+    printf("\t░▒▓█▓▒░      ░▒▓█▓▒░░▒▓██████▓▒░  ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░\n");
+    printf("\tFerramenta Interativa para Controle de Trafego em Aeroportos\n");
+    printf("\n");
+}
+
+void
+io_menu(void)
+{
+    printf("\nEscolha uma das seguintes opcoes:\n\n");
+    printf("a\tCadastrar um novo voo na fila de espera\n");
+    printf("b\tCadastrar um passageiro em um voo\n");
+    printf("c\tRemover um passageiro de um voo\n");
+    printf("d\tVer detalhes do proximo aviao a decolar\n");
+    printf("e\tAutorizar o proximo aviao a decolar\n");
+    printf("f\tVerificar numero de avioes aguardando decolagem\n");
+    printf("g\tListar todos os avioes aguardando decolagem\n");
+    printf("h\tListar todos os passageiros de um voo\n");
+    printf("q\tSair do voo\n\n");
+    printf("> ");
+}
+
+// Binary tree functions--------------------------------------------------------
+
 struct btree
 {
     char p_name[40];
@@ -28,9 +83,16 @@ struct btree
     struct btree *p_r;
 };
 
-struct btree *bt_cr_leaf(char *p_name_new)
+struct btree *
+bt_cr_leaf(char *p_name_new)
 {
     struct btree *p_new = malloc(sizeof(struct btree));
+    if (p_name_new == NULL)
+    {
+        printf("\nSem memoria\n");
+        system("pause");
+        exit(1);
+    }
 
     strcpy(p_new->p_name, p_name_new);
 
@@ -40,7 +102,8 @@ struct btree *bt_cr_leaf(char *p_name_new)
     return p_new;
 }
 
-int bt_rec_insert(struct btree **p_root, char *p_name_new)
+int
+bt_rec_insert(struct btree **p_root, char *p_name_new)
 {
     // First node ever
     if (*p_root == NULL)
@@ -58,6 +121,7 @@ int bt_rec_insert(struct btree **p_root, char *p_name_new)
     {
         return bt_rec_insert(&((*p_root)->p_r), p_name_new);
     }
+    // Print to screen if name to be inserted already exists, return 1
     else
     {
         printf("Nome ja inserido\n");
@@ -65,16 +129,18 @@ int bt_rec_insert(struct btree **p_root, char *p_name_new)
     }
 }
 
+// List functions---------------------------------------------------------------
+
 struct node
 {
     struct node *p_nxt;   // Next element in queue
     char p_pid[8];        // Plane ID
-    char p_dtn[4];        // Flight DesTiNation
+    char p_dst[4];        // Flight DesTiNation
     char p_cmp[20];       // Flight CoMPany
     char p_reg[9];        // Plane REGister
     char p_mdl[20];       // Plane MoDeL
     int  nst;             // Number os SeaTs
-    struct btree *p_pl;   // Pointer to the Passenger List
+    struct btree *p_pal;   // Pointer to the Passenger List
 };
 
 struct queue
@@ -83,26 +149,66 @@ struct queue
     struct node *p_t; // Pointer to queue Tail
 };
 
-int main(void)
+struct node *
+qu_cr_node()
 {
-    printf("Inicio do teste\n");
-    struct node teste;
+    struct node *p_new_nd = (struct node *)malloc(sizeof(struct node));
+    if (p_new_nd == NULL)
+    {
+        printf("\nSem memoria\n");
+        system("pause");
+        exit(1);
+    }
 
-    teste.p_nxt = NULL;
-    strcpy(teste.p_pid, "GLO1123");
-    strcpy(teste.p_dtn, "CGH");
-    strcpy(teste.p_cmp, "GOL");
-    strcpy(teste.p_reg, "PR-XMW");
-    strcpy(teste.p_mdl, "Boeing 737-7K5");
-    teste.nst = 138;
-    teste.p_pl = NULL;
-
-    char n1[] = "Igor";
-    char n2[] = "Marina";
-
-    bt_rec_insert(&(teste.p_pl), n1);
-    bt_rec_insert(&(teste.p_pl), n2);
-    bt_rec_insert(&(teste.p_pl), n1);
+    p_new_nd->p_nxt = NULL;
+    strcpy(p_new_nd->p_pid, "0000000");
+    strcpy(p_new_nd->p_dst, "000");
+    strcpy(p_new_nd->p_cmp, "0000000000000000000");
+    strcpy(p_new_nd->p_reg, "00000000");
+    strcpy(p_new_nd->p_mdl, "0000000000000000000");
+    p_new_nd->nst = 0;
+    p_new_nd->p_pal = NULL;
 
     return 0;
+}
+
+struct queue *
+qu_cr_queue()
+{
+    struct queue *p_new_qu = (struct queue *)calloc(1, sizeof(struct queue));
+    if (p_new_qu == NULL)
+    {
+        printf("\nSem memoria\n");
+        exit(1);
+    }
+
+    return p_new_qu;
+}
+
+int
+main(void)
+{
+    io_startup();
+    io_menu();
+    io_str_input();
+    // printf("Inicio do teste\n");
+    // struct node teste;
+
+    // teste.p_nxt = NULL;
+    // strcpy(teste.p_pid, "GLO1123");
+    // strcpy(teste.p_dst, "CGH");
+    // strcpy(teste.p_cmp, "GOL");
+    // strcpy(teste.p_reg, "PR-XMW");
+    // strcpy(teste.p_mdl, "Boeing 737-7K5");
+    // teste.nst = 138;
+    // teste.p_pal = NULL;
+
+    // char n1[] = "Igor";
+    // char n2[] = "Marina";
+
+    // bt_rec_insert(&(teste.p_pal), n1);
+    // bt_rec_insert(&(teste.p_pal), n2);
+    // bt_rec_insert(&(teste.p_pal), n1);
+
+    // return 0;
 }
